c8d20e1f3d0087167e20196c60830a1b11d24e7b
bubu1441006 <hoangdung.bubu@gmail.com>
2021-11-28 18:22:40 +0700

    Change to parallel

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.BallShooter;
@@ -15,7 +15,7 @@ import static frc.robot.Constants.AUTO_CONSTANTS.*;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class Auto extends SequentialCommandGroup {
+public class Auto extends ParallelCommandGroup {
   public Auto(BallSucker ballSucker, BallShooter ballShooter, DriveBase driveBase) {
 
     addCommands(
